772c0c38e435fb87a08722d8f9e9060887a724ec
Julien Bouyoud <jBouyoud@users.noreply.github.com>
2020-11-07 00:06:09 +0100

    feat: implement 'remove' command
    Signed-off-by: Julien Bouyoud <jBouyoud@users.noreply.github.com>

--- tests/lib/asserts-config-repository.bash
+++ tests/lib/asserts-config-repository.bash
@@ -1,7 +1,14 @@
 
+assert_config_repository_scheme_not_exist() {
+    file="${HELM_CONFIG_SCHEME_REPOSITORY}/${1}"
+
+     assert_file_not_exist "${file}"
+}
+
 assert_config_repository_scheme() {
     file="${HELM_CONFIG_SCHEME_REPOSITORY}/${1}"
 
     assert_file_exist "${file}"
     assert_equal "$(cat "${file}")" "${2}"
 }
+
